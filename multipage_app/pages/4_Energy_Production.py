ef72a05ac4e3ef752277c0c14dd6cb8825ce7104
Izza Qamar <izzaqamarbci@gmail.com>
2025-10-23 22:41:46 +0200

    edited comments

--- multipage_app/pages/4_Energy_Production.py
+++ multipage_app/pages/4_Energy_Production.py
@@ -30,37 +30,42 @@ mongodb_df = pd.DataFrame(all_items)
 #Getting the time in UTC format
 mongodb_df['startTime'] = pd.to_datetime(mongodb_df['startTime'], utc=True)
 
-st.markdown("## **Visualisations in this dashboard are for the data fetched from MongoDB**")
+st.markdown(" ## **Visualisations of data fetched from MongoDB**")
 
 #Using st.columns to split view in 2 parts
 col_left,col_right=st.columns(2)
 
-#For the left side conatining pie plot
+#For the left side containing pie plot
 with col_left:
-    st.header("A pie plot")
+    st.subheader("A pie plot")
     st.write("") 
     st.write("View the total production of different energy groups for the year 2021 in the selected area.")
-    price_areas = mongodb_df['priceArea'].unique().tolist()
-    selected_price_area=st.radio("Select an area:",price_areas)
-    filtered_df_area=mongodb_df[mongodb_df["priceArea"]==selected_price_area]\
-    .groupby("productionGroup").agg(total_production=("quantityKwh", "sum")).reset_index()
     
-    #Creating a Plotly pie chart
-    fig = px.pie(filtered_df_area,
-        names="productionGroup",   
-        values="total_production",  
-        title=(f"Pie Plot of {selected_price_area} Production Quantity"))
+    price_areas = sorted(mongodb_df['priceArea'].unique())
+    selected_price_area=st.radio("Select an area:",price_areas)
 
-    #Showing the figure
-    fig.update_traces(textinfo='percent+label',textposition='outside',pull=[0.1, 0.1, 0],textfont_size=12)
-    fig.update_layout(showlegend=True,margin=dict(t=50, b=50, l=50, r=50),width=700,height=700)
-    #Displaying the pie chart
-    st.plotly_chart(fig, use_container_width=True) 
+    if selected_price_area:
+        filtered_df_area=mongodb_df[mongodb_df["priceArea"]==selected_price_area]\
+        .groupby("productionGroup").agg(total_production=("quantityKwh", "sum")).reset_index()
+        
+        #Creating a Plotly pie chart
+        fig = px.pie(filtered_df_area,
+            names="productionGroup",   
+            values="total_production",  
+            title=(f"Pie Plot of {selected_price_area} Production Quantity"))
+
+        #Showing the figure
+        fig.update_traces(textinfo='percent+label',textposition='outside',pull=[0.1, 0.1, 0],textfont_size=12)
+        fig.update_layout(showlegend=True,margin=dict(t=50, b=50, l=50, r=50),width=700,height=700)
+        #Displaying the pie chart
+        st.plotly_chart(fig, use_container_width=True) 
+    else:
+        st.warning("Please select a price area to display the pie plot.")
 
 
 #For the right side containing line plot
 with col_right:
-    st.header("A line plot")
+    st.subheader("A line plot")
     st.write("") 
     st.write("Visualize the production trends of selected production groups for the chosen area and month.")
     #Extracting all the production groups in a list
@@ -79,23 +84,25 @@ with col_right:
     #Adding a selctbox to choose any month
     selected_month = st.selectbox("Select a month:", months)
 
+    if selected_group:
+        #Filtering data for selected price area,month and selected group and then grouping and aggregating accordingly
+        filtered_df_month_group_area=mongodb_df[(mongodb_df["priceArea"]==selected_price_area)&(mongodb_df["productionGroup"].isin(selected_group))&(mongodb_df["month"]==selected_month)]
+        grouped_df_month_group=filtered_df_month_group_area.groupby(["startTime","productionGroup"]).agg(total_production=("quantityKwh", "sum")).reset_index()
+        
+        #Plotting line chart using plotly
+        fig = px.line(grouped_df_month_group,x='startTime',y='total_production',
+            color='productionGroup',
+            title=f"Energy Production in Area { selected_price_area} in {selected_month}",
+            labels={'startTime': 'Time','total_production': 'Production (kWh)','productionGroup': 'Production Group'},template='plotly_white')
 
-    #Filtering data for selected price area,month and selected group and then grouping and aggregating accordingly
-    filtered_df_month_group_area=mongodb_df[(mongodb_df["priceArea"]==selected_price_area)&(mongodb_df["productionGroup"].isin(selected_group))&(mongodb_df["month"]==selected_month)]
-    grouped_df_month_group=filtered_df_month_group_area.groupby(["startTime","productionGroup"]).agg(total_production=("quantityKwh", "sum")).reset_index()
-    
-    #Plotting line chart using plotly
-    fig = px.line(grouped_df_month_group,x='startTime',y='total_production',
-        color='productionGroup',
-        title=f"Energy Production in Area { selected_price_area} in {selected_month}",
-        labels={'startTime': 'Time','total_production': 'Production (kWh)','productionGroup': 'Production Group'},template='plotly_white')
+        #Customizing axes & layout
+        fig.update_layout(width=1000,height=600,margin=dict(t=60, b=60, l=60, r=60),legend_title_text="Production Group")
+        fig.update_xaxes(tickformat='%d-%m-%Y', tickangle=45)
 
-    #Customizing axes & layout
-    fig.update_layout(width=1000,height=600,margin=dict(t=60, b=60, l=60, r=60),legend_title_text="Production Group")
-    fig.update_xaxes(tickformat='%Y-%m-%d %H:%M',tickangle=45)
-
-    #Displaying chart in Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+        #Displaying chart in Streamlit
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.warning("Please select at least one production group to display the line plot.")
 
 #Using st.expander to show the source of data
 with st.expander("See source of the data"):
